9ed41b56b131e362b86040f79fd6e3763fa60d6f
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-08-08 22:10:50 +0800

    resolve deadlock

--- src/main/java/me/kall/bedload/mixin/ServerLevelMixin.java
+++ src/main/java/me/kall/bedload/mixin/ServerLevelMixin.java
@@ -5,6 +5,7 @@ import me.kall.bedload.api.ChunkLoader;
 import me.kall.bedload.config.BedLoadConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
@@ -15,15 +16,28 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nonnull;
+
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
     @Shadow public abstract boolean setChunkForced(int chunkX, int chunkZ, boolean add);
+
+    @Shadow @Nonnull public abstract MinecraftServer getServer();
 
     @Inject(
             method = "onBlockStateChange",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;immutable()Lnet/minecraft/core/BlockPos;")
     )
     private void onChunkLoaderUpdate(BlockPos pos, BlockState oldState, BlockState newState, CallbackInfo ci) {
+        if (!this.getServer().isSameThread()) {
+            this.getServer().execute(() -> bedLoad$execute(pos, oldState, newState));
+            return;
+        }
+        bedLoad$execute(pos, oldState, newState);
+    }
+
+    @Unique
+    private void bedLoad$execute(BlockPos pos, BlockState oldState, BlockState newState) {
         final int chunkX = SectionPos.blockToSectionCoord(pos.getX());
         final int chunkZ = SectionPos.blockToSectionCoord(pos.getZ());
 
